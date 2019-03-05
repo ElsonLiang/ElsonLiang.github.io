19827779203c29d31b1a248c966c0cda64b6948f
ElsonLiang <elsonl2212@gmail.com>
2019-03-04 17:55:58 -0800

    made a game

---
+++ main.js
@@ -0,0 +1,13 @@
+let food = window.prompt('give me a food name(plural)')
+console.log(food)
+
+document.getElementById('food').innerText = food;
+
+let enemy = window.prompt('Who is someone you hate?')
+document.getElementById('enemy').innerText = enemy;
+
+let friend = window.prompt('What is you friends name?')
+document.getElementById('friend').innerText = friend;
+
+let verb = window.prompt('What is something you enjoy doing(one word answer with the ing)')
+document.getElementById('verb').innerText = verb;
