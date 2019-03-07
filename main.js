98252ec72e942d59df9deb609a975c7881efc049
ElsonLiang <elsonl2212@gmail.com>
2019-03-06 17:31:32 -0800

    made buttons

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-let food = window.prompt('give me a food name(plural)')
 console.log(food)
 
-document.getElementById('food').innerText = food;
+function ChangeFood(){
+    let food = window.prompt('give me a food name(plural)')
+    document.getElementById('food').innerText = food;
+}
+document.getElementById('ChangeFood').onclick = ChangeFood
 
+function ChangeEnemy(){
 let enemy = window.prompt('Who is someone you hate?')
 document.getElementById('enemy').innerText = enemy;
+}
+document.getElementById('ChangeEnemy').onclick = ChangeEnemy
 
+function ChangeFriend(){
 let friend = window.prompt('What is you friends name?')
 document.getElementById('friend').innerText = friend;
+}
+document.getElementById('ChangeFriend').onclick = ChangeFriend
 
+function ChangeVerb(){
 let verb = window.prompt('What is something you enjoy doing(one word answer with the ing)')
 document.getElementById('verb').innerText = verb;
+}
+document.getElementById('ChangeVerb').onclick = ChangeVerb
