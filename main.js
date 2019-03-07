9b4a1a23a865fd20b5bcea1b35c8dc66c4c2c9f1
ElsonLiang <elsonl2212@gmail.com>
2019-03-06 17:59:15 -0800

    made number button

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 console.log(food)
 
 function ChangeFood(){
-    let food = window.prompt('give me a food name(plural)')
+    let food = window.prompt('give me a food name(plural)');
     document.getElementById('food').innerText = food;
 }
-document.getElementById('ChangeFood').onclick = ChangeFood
+document.getElementById('ChangeFood').onclick = ChangeFood;
 
 function ChangeEnemy(){
-let enemy = window.prompt('Who is someone you hate?')
+let enemy = window.prompt('Who is someone you hate?');
 document.getElementById('enemy').innerText = enemy;
 }
-document.getElementById('ChangeEnemy').onclick = ChangeEnemy
+document.getElementById('ChangeEnemy').onclick = ChangeEnemy;
 
 function ChangeFriend(){
-let friend = window.prompt('What is you friends name?')
+let friend = window.prompt('What is you friends name?');
 document.getElementById('friend').innerText = friend;
 }
-document.getElementById('ChangeFriend').onclick = ChangeFriend
+document.getElementById('ChangeFriend').onclick = ChangeFriend;
 
 function ChangeVerb(){
 let verb = window.prompt('What is something you enjoy doing(one word answer with the ing)')
 document.getElementById('verb').innerText = verb;
 }
-document.getElementById('ChangeVerb').onclick = ChangeVerb
+document.getElementById('ChangeVerb').onclick = ChangeVerb;
+
+let value=0;
+
+function subtract(){
+    value=value-1;
+}
+document.getElementById('subtract').onclick = subtract;
+
+function add(){
+    value=value+1;
+    console.log(value);
+}
+document.getElementById('add').onclick = add;
